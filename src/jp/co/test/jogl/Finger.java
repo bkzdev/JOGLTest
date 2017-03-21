f14aa94105f3c6839e1a31299b471939e9f6a345
nagata <nagata@ann.com>
2017-03-21 14:51:54 +0900

    10.階層構造 FingerSampleNewtのみ

---
+++ src/jp/co/test/jogl/Finger.java
@@ -0,0 +1,41 @@
+package jp.co.test.jogl;
+
+import com.jogamp.opengl.GL2;
+
+/**
+ * @author 
+ * 
+ * http://toruwest.github.io/jogl-tutorial-document/<br>
+ * 10.階層構造
+ * 未作成
+ *
+ */
+public class Finger {
+
+	public Finger(float f, float g, float h, float i, float[] red) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public Finger(Finger finger, float f, float g, float[] blue) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public void updateRotation(float rotDelta) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	public void render(GL2 gl2) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
---
+++ src/jp/co/test/jogl/Palm.java
@@ -0,0 +1,32 @@
+package jp.co.test.jogl;
+
+import com.jogamp.opengl.GL2;
+
+/**
+ * @author 
+ * 
+ * http://toruwest.github.io/jogl-tutorial-document/<br>
+ * 10.階層構造
+ * 未作成
+ *
+ */
+public class Palm {
+
+	public Palm(int i, int j, int k, float[] orange) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public void render(GL2 gl2) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+}
